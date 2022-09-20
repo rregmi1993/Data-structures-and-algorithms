3cef0548fa550a7105d25086e4ab3a789b677edc
Ramchandra Regmi <46373964+rregmi1993@users.noreply.github.com>
2022-09-20 23:06:47 +0530

    using linkedList

---
+++ graph/DirectedGraphUsingLinkedList.java
@@ -0,0 +1,43 @@
+package graph;
+
+import java.util.LinkedList;
+
+public class DirectedGraphUsingLinkedList {
+	
+	int vertices;
+	LinkedList<Integer>[] adjList;
+	DirectedGraphUsingLinkedList(int vertices){
+		this.vertices = vertices;
+		this.adjList = new LinkedList[vertices];
+		for(int i=0; i<vertices; i++) {
+			adjList[i] = new LinkedList<>();
+		}
+		
+	}
+	
+	void addEdge(int i, int j) {
+		adjList[i].add(j);
+	}
+	
+	public void display() {
+		for(int i=0; i<vertices; i++) {
+			System.out.print("Vertex " + i + ": ");
+			for (int j = 0; j < adjList[i].size(); j++) {
+				System.out.print("->" + adjList[i].get(j));
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DirectedGraphUsingLinkedList ugl = new DirectedGraphUsingLinkedList(5);
+		ugl.addEdge(0, 1);
+		ugl.addEdge(0, 4);
+		ugl.addEdge(0, 3);
+		ugl.addEdge(1, 2);
+		ugl.addEdge(1, 3);
+		ugl.addEdge(2, 3);
+		ugl.addEdge(3, 4);
+		ugl.display();
+	}
+}
